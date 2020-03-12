d5e0ea11542377357686c3ae35ba8e699d19f4d8
Wei Zhang <wjzhang@uci.edu>
2020-03-11 18:20:14 -0700

    initial commit for bejeweled

---
+++ src/main/java/jfx/game/Library/Player.java
@@ -0,0 +1,17 @@
+public class Player {
+    String name;
+    int points;
+
+    public Player(String name, int points) {
+        this.name = name;
+        this.points = points;
+    }
+
+    public String toString() {
+        return (this.name + " " + this.points);
+    }
+
+    public int getPoints(){
+        return this.points;
+    }
+}
