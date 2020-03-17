d4e8a783cc485473a149f0ca0c4362e72f31befb
Wei Zhang <wjzhang@uci.edu>
2020-03-17 13:14:24 -0700

    added 4 other jewels

--- src/main/java/jfx/game/Library/Bejeweled/Rectangle.java
+++ src/main/java/jfx/game/Library/Bejeweled/Rectangle.java
@@ -7,9 +7,9 @@ import jfx.game.Library.Bejeweled.VerticalMatch;
 import jfx.game.Library.Bejeweled.Coordinate;
 import jfx.game.Library.Bejeweled.JewelMatch;
 
-public class Diamond extends Jewel{
-    public Diamond(int x, int y) {
-        super("D", 30, new Coordinate(x, y));
+public class Square extends Jewel {
+    public Square(int x, int y) {
+        super("S", 10, new Coordinate(x, y));
         this.jewelHorizontalMatch = new HorizontalMatch(new VerticalMatch());
         addMatchingJewelNames(this.name);
     }
