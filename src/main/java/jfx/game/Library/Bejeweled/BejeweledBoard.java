1409c0e109ae9dd734982473e8730da8de4609d7
Ki Min Lee <thekiminlee@gmail.com>
2020-03-19 22:24:42 -0700

    Fix swap

--- src/main/java/jfx/game/Library/Bejeweled/BejeweledBoard.java
+++ src/main/java/jfx/game/Library/Bejeweled/BejeweledBoard.java
@@ -3,6 +3,7 @@ package jfx.game.Library.Bejeweled;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Random;
+import java.lang.Math;
 
 import javafx.application.Platform;
 import tmge.engine.gameComponents.Board;
@@ -12,101 +13,115 @@ import tmge.engine.gameComponents.TileGame;
 import tmge.engine.gameComponents.TileGenerator;
 
 public class BejeweledBoard extends Board {
-    private int score;
-    private int[][][] configurations = {{{0,0}},{{0,0}},{{0,0}},{{0,0}}};
-    private int[] values = {1,2,3,4};
-    private static final int ROWS = 8, COLUMNS = 8;
-    private ArrayList<Coordinate> listOfCoords;
-    private TileGenerator generator;
-    private boolean playing;
-    private Random seed;
-    private Tile t1, t2 = null;
-    private int clicked = 0;
+	private int score;
+	private int[][][] configurations = { { { 0, 0 } }, { { 0, 0 } }, { { 0, 0 } }, { { 0, 0 } } };
+	private int[] values = { 1, 2, 3, 4 };
+	private static final int ROWS = 8, COLUMNS = 8;
+	private ArrayList<Coordinate> listOfCoords;
+	private TileGenerator generator;
+	private boolean playing;
+	private Random seed;
+	private Tile t1, t2 = null;
+	private int clicked = 0;
 
-    BejeweledScreen screen;
+	BejeweledScreen screen;
 
-    public BejeweledBoard(BejeweledScreen screen){
-    	super(new TileGame(ROWS, COLUMNS));
+	public BejeweledBoard(BejeweledScreen screen) {
+		super(new TileGame(ROWS, COLUMNS));
 		seed = new Random(LocalTime.now().toNanoOfDay());
 		this.screen = screen;
 		generator = screen.getGenerator();
 		generator.setGridDimensions(ROWS, COLUMNS);
 		for (int row = 0; row < ROWS; row++)
-			for (int col = 0; col < COLUMNS; col++){
-				//Coordinate c = new Coordinate(row, col);
+			for (int col = 0; col < COLUMNS; col++) {
+				// Coordinate c = new Coordinate(row, col);
 				board[row][col] = generator.emptyTile();
-				//listOfCoords.add(new Coordinate(row, col));
+				// listOfCoords.add(new Coordinate(row, col));
 			}
 		playing = true;
 		System.out.println("Board created");
-    }
+	}
 
-    //Matching on board
-    public int applyMatch(ArrayList<Coordinate> list) {
-        int points = 0;
-        for (Coordinate coords : list) {
-            Tile tile = board[coords.getX()][coords.getY()];
-            points += tile.getValue();
-            System.out.println(list);
+	// Matching on board
+	public int applyMatch(ArrayList<Coordinate> list) {
+		int points = 0;
+		for (Coordinate coords : list) {
+			Tile tile = board[coords.getX()][coords.getY()];
+			points += tile.getValue();
+			System.out.println(list);
 
-            // Set empty Tile at the coordinate
-            removeTile(coords.getX(), coords.getY());
-        }
+			// Set empty Tile at the coordinate
+			removeTile(coords.getX(), coords.getY());
+		}
 
-        return points;
-    }
+		return points;
+	}
 
-    //GRAVITY OF THE GAME THAT HANDLES DROPPING
-    public void applyGravity() {
-    	for (int row = this.getRows() - 1; row > 0; row++) {
-    		for (int column = this.getColumns() - 1; column >= 0; column++) {
-    			if (board[row][column] == null)
-    				removeTile(row, column);
-    			else if (board[row][column] == generator.emptyTile()) {
-    				addTile(row, column, board[row - 1][column]);
-    				removeTile(row - 1, column);
-    			}
-    		}
-    	}
-    	fillRow(0);
-    }
-    
-    public void fillRow(int row) {
-    	for (int column = 0; column < this.getColumns(); column++) {
-    		int rand = seed.nextInt(configurations.length);
-    		if (!occupied(board[row][column])) {
-    			board[row][column] = generator.createCustomTile(rand,
-					rand, values[rand], new Coordinate(row, column));
-    		}
-    	}
-    }
-    
-    public void fillAll() {
-    	for(int row = 0; row < this.getRows(); row++){
+	// GRAVITY OF THE GAME THAT HANDLES DROPPING
+	public void applyGravity() {
+		for (int row = this.getRows() - 1; row > 0; row++) {
+			for (int column = this.getColumns() - 1; column >= 0; column++) {
+				if (board[row][column] == null)
+					removeTile(row, column);
+				else if (board[row][column] == generator.emptyTile()) {
+					addTile(row, column, board[row - 1][column]);
+					removeTile(row - 1, column);
+				}
+			}
+		}
+		fillRow(0);
+	}
+
+	public void fillRow(int row) {
+		for (int column = 0; column < this.getColumns(); column++) {
+			int rand = seed.nextInt(configurations.length);
+			if (!occupied(board[row][column])) {
+				board[row][column] = generator.createCustomTile(rand, rand, values[rand], new Coordinate(row, column));
+			}
+		}
+	}
+
+	public void fillAll() {
+		for (int row = 0; row < this.getRows(); row++) {
 			fillRow(row);
 		}
 	}
 
-	public void swap(Coordinate coords, Coordinate lastClicked){
-		Tile temp1 = getTileAt(coords), temp2 = getTileAt(lastClicked);
-		temp1.setCoords(lastClicked);
-		setTileAt(lastClicked, temp1);
-		temp2.setCoords(coords);
-		setTileAt(coords, temp2);
+	private boolean isValid(Coordinate coords, Coordinate lastClicked) {
+		if ((Math.abs(coords.getX() - lastClicked.getX()) == 1) && (coords.getY() == lastClicked.getY()))
+			return true;
+		else if ((Math.abs(coords.getY() - lastClicked.getY()) == 1) && (coords.getX() == lastClicked.getX()))
+			return true;
+		else if ((Math.abs(coords.getX() - lastClicked.getX()) == 1)
+				&& (Math.abs(coords.getY() - lastClicked.getY()) == 1))
+			return true;
+		return false;
 	}
-	
+
+	public void swap(Coordinate coords, Coordinate lastClicked) {
+		if (isValid(coords, lastClicked)) {
+			Tile temp1 = getTileAt(coords), temp2 = getTileAt(lastClicked);
+			temp1.setCoords(lastClicked);
+			setTileAt(lastClicked, temp1);
+			temp2.setCoords(coords);
+			setTileAt(coords, temp2);
+		} else {
+			System.out.println("Invalid move");
+		}
+	}
+
 	public Tile getTileAt(Coordinate coords) {
 		return board[coords.getX()][coords.getY()];
 	}
-	
+
 	public void setTileAt(Coordinate coords, Tile t) {
 		board[coords.getX()][coords.getY()] = t;
 	}
 
-    public boolean occupied(Tile t) {
-    	return (t != null) && (t != generator.emptyTile());
-    }
-    
+	public boolean occupied(Tile t) {
+		return (t != null) && (t != generator.emptyTile());
+	}
+
 	@Override
 	public void addTile(int row, int column, Tile t) {
 		if (t == null)
@@ -123,8 +138,8 @@ public class BejeweledBoard extends Board {
 	@Override
 	public void update() {
 
-    	while(playing) {
-//			System.out.println("UPDATE IN BJ board");
+		while (playing) {
+			// System.out.println("UPDATE IN BJ board");
 			this.screen.setReady(false);
 
 			Platform.runLater(() -> {
@@ -132,7 +147,7 @@ public class BejeweledBoard extends Board {
 				this.screen.draw();
 			});
 
-			while(!this.screen.ready() && playing)
+			while (!this.screen.ready() && playing)
 				try {
 					Thread.sleep(1000);
 				} catch (InterruptedException e) {
@@ -143,7 +158,7 @@ public class BejeweledBoard extends Board {
 
 	@Override
 	public void run() {
-    	System.out.println("I am in run");
+		System.out.println("I am in run");
 		update();
 	}
 
--- src/main/java/tmge/engine/userComponents/Score.java
+++ src/main/java/tmge/engine/userComponents/Score.java
@@ -4,7 +4,7 @@ public class Score {
 	public int bejeweledScore;
 	public int tetrisScore;
 	public String userName;
-	
+
 	Score(String name, int bjScore, int tetScore) {
 		userName = name;
 		bejeweledScore = bjScore;
