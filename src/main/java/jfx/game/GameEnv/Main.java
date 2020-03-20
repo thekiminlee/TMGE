5498c0219d07fc5c0a0b1f65989f569bfa0e1af4
Ki Min Lee <thekiminlee@gmail.com>
2020-03-20 15:49:19 -0700

    Add game reset and game end functionality

--- src/main/java/jfx/game/GameEnv/Main.java
+++ src/main/java/jfx/game/GameEnv/Main.java
@@ -6,7 +6,7 @@ import jfx.game.Library.App;
  * A pseudo javafx loader class, bypasses some errors in creating a jarfile.
  */
 public class Main {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         App.main(args);
     }
 }
